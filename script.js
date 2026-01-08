0862214dacb795307386b6028490ae5cc0caedb1
Chennakesava Reddy Battula <bckr2003@gmail.com>
2026-01-08 10:27:21 +0530

    Day 7: Started using Javascript and got know about varibales and datatypes

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Welcome to my website!");
+const name="Battula Chennakeshava Reddy";
+let age=22;
+const goal="To become financially independent and successful in my career by age of 40.";
+let s=true;
+console.log(s)
+console.log("Name: ",name);
+console.log("Age: ",age);
+console.log("Goal:",goal);
+
+age=age+1;
+console.log("Next Year Age: ",age);
