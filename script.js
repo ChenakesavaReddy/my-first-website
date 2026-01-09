bc161be4595e44c61960c8feb33109f511f2e73f
Chennakesava Reddy Battula <bckr2003@gmail.com>
2026-01-09 10:06:48 +0530

    Day 8: Learned about if else statements and used in the code

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+alert("Welcome to my website");
 const name="Battula Chennakeshava Reddy";
 let age=22;
 const goal="To become financially independent and successful in my career by age of 40.";
 let s=true;
-console.log(s)
+
+console.log(s);
 console.log("Name: ",name);
 console.log("Age: ",age);
+console.log("Goal:",goal);
 
 function calculateAge(age){
     return age+1;
 }
 console.log(calculateAge(age));
 
-console.log("Goal:",goal);
 age=age+1;
 console.log("Next Year Age: ",age);
 
@@ -21,3 +23,17 @@ function greet(name){
 greet(name);
 
 
+function checkaccess(age){
+    if(age<13){
+        console.log("Your are a chile");
+    }
+    else if(age<22){
+        console.log("Your are a teenager");
+    }
+    else{
+        console.log("Your are a adult");
+    }
+}
+
+myage=26;
+checkaccess(myage);
